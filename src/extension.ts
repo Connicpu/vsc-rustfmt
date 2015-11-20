6a270221085226c861545d0889b6d97df6c98d54
Jamison Greeley <berwyn.codeweaver@gmail.com>
2015-11-20 13:50:36 -0700

    feat(editor): format-on-save, configuration, style
    1. Adds format-on-save, (optionally) formatting Rust documents as they're
    saved.
    2. Adds configuration options to enable/disable format-on-save, as well as
    the path to the rustfmt binary.
    3. Updates the command to use the style established in Microsoft repos.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,34 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
+let config;
+
+function formatDocument(document: string) {
+	let bin = config.bin === '' ? 'rustfmt' : config.bin;
+	let result = child_process.spawnSync(bin, ['--write-mode', 'overwrite', document]);
+	if (result.error) {
+		vscode.window.showErrorMessage(result.error.toString());
+	} else if(result.stderr.toString().length != 0) {
+		let channel = vscode.window.createOutputChannel('Rustfmt Output');
+		channel.append(result.stderr.toString());
+		channel.show();
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
-	var disposable = vscode.commands.registerTextEditorCommand("extension.rustfmt", (editor: vscode.TextEditor) => {
-		editor.document.save().then((fulfilled: boolean) => {
-			let file = editor.document.fileName;
-			let result = child_process.spawnSync("rustfmt", ["--write-mode", "overwrite", file]);
-			
-			if (result.error) {
-				vscode.window.showErrorMessage(result.error.toString());
-			} else {
-				if (result.stderr.toString().length != 0) {
-					vscode.window.showErrorMessage("Something went wrong during formatting");
-				} else {
-					vscode.window.showInformationMessage("Formatted!");
-				}
-			}
-		});
-	});
+	config = vscode.workspace.getConfiguration('rustfmt');
 	
-	context.subscriptions.push(disposable);
+	// Manual fmt command
+	context.subscriptions.push(vscode.commands.registerTextEditorCommand('rustfmt.fmt', editor => {
+		editor.document.save().then(fulfilled => {
+			formatDocument(editor.document.fileName);
+		});
+	}));
+	
+	// Automatic save handler
+	context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(target => {
+		if(/\.rs$/.test(target.fileName) && (config.formatOnSave || true)) {
+			formatDocument(target.fileName);
+		}
+	}));
 }
