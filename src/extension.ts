30fbe6ffd30e8b9cf62c1a4d50903d3537e7d99a
Peter Arato <it.arato@gmail.com>
2017-01-10 14:40:20 +0800

    Make missing bin check universal.

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as child_process from 'child_process';
 let config;
 
 function formatDocument(document: string) {
-	let bin = config.bin === '' ? 'rustfmt' : config.bin;
+	let bin = !config.bin ? 'rustfmt' : config.bin;
 	let result = child_process.spawnSync(bin, ['--write-mode', 'overwrite', document]);
 	if (result.error) {
 		vscode.window.showErrorMessage(result.error.toString());
@@ -17,14 +17,14 @@ function formatDocument(document: string) {
 
 export function activate(context: vscode.ExtensionContext) {
 	config = vscode.workspace.getConfiguration('rustfmt');
-	
+
 	// Manual fmt command
 	context.subscriptions.push(vscode.commands.registerTextEditorCommand('rustfmt.fmt', editor => {
 		editor.document.save().then(fulfilled => {
 			formatDocument(editor.document.fileName);
 		});
 	}));
-	
+
 	// Automatic save handler
 	context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(target => {
 		if(/\.rs$/.test(target.fileName) && (config.formatOnSave || true)) {
